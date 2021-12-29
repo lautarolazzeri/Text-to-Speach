3fdc756ff43c3c0236b67dcce2ba92cd00ec1cfd
Lautaro <lautyylazzerii@gmail.com>
2021-12-29 14:58:50 -0300

    changes

--- index.js
+++ index.js
@@ -8,28 +8,30 @@ var recognition = new webkitSpeechRecognition();
 recognition.continuous = true;
 recognition.interimResults = true;
 
-var start = function() {
-  results.innerHTML = '';
-  interim.innerHTML = '';
-  final_transcript = '';
-  interim_transcript = '';
-  recognition.start();
-  
-  recognition.onresult = function(event) {
+var start = function () {
+    results.innerHTML = '';
+    interim.innerHTML = '';
+    final_transcript = '';
     interim_transcript = '';
     
-    for (var i = event.resultIndex; i < event.results.length; ++i) {
-      if (event.results[i].isFinal) {
-        final_transcript += event.results[i][0].transcript;
-      } else {
-        interim_transcript += event.results[i][0].transcript;
-      }
-    }
-    results.innerHTML = final_transcript;
-    interim.innerHTML = "Currently Saying: " + interim_transcript;
-  };
+    recognition.start();
+
+    recognition.onresult = function (event) {
+        interim_transcript = '';
+
+        for (var i = event.resultIndex; i < event.results.length; ++i) {
+            if (event.results[i].isFinal) {
+                final_transcript += event.results[i][0].transcript;
+            } else {
+                interim_transcript += event.results[i][0].transcript;
+            }
+        }
+
+        results.innerHTML = final_transcript;
+        interim.innerHTML = "Currently Saying: " + interim_transcript;
+    };
 };
 
-var stop = function() {
-  recognition.stop();
+var stop = function () {
+    recognition.stop();
 };
